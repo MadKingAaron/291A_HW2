8d6c1acc89ba1bc02d256e2cd4661a0fa3833651
Aaron Jimenez <arjl5012@gmail.com>
2023-03-13 16:25:53 -0700

    Added args and modified main for trades training

--- evaluate.py
+++ evaluate.py
@@ -42,6 +42,13 @@ def parse_args():
         '--data_dir', default='./data/', type=str, help="Folder to store downloaded dataset"
     )
     parser.add_argument("--checkpt_freq", type=int, default=20, help="Frequency to save model checkpoints")
+
+    parser.add_argument("--trades", action='store_true')
+
+    parser.add_argument("--unsupervised", action='store_true')
+
+    parser.add_argument("--gamma", type=float, default=0.001, help="Gamma value for TRADES")
+
     parser.add_argument(
         '--save_model', action='store_true'
     )
@@ -51,6 +58,7 @@ def parse_args():
     parser.add_argument(
         '--log_dir', type=str, default='./run/training_log', help="Output directory for TensorBoard"
     )
+
     parser.add_argument("--load_checkpoint", type=str, default="", help="(Optional) Load checkpoint dir")
     parser.add_argument("--targeted", action='store_true')
     parser.add_argument("--device", type=str, default="cpu", help="Device to use")
@@ -81,11 +89,20 @@ def main():
         attacker = attack_util.FGSMAttack(eps=eps, loss_type=args.attack_loss_type, targeted=args.targeted,
             num_classes=10)
     
-    # Get trainer object
-    trainer = train_model.ModelTrainer(model, attacker, train_loader, args.lr, args.optimizer, device=args.device)
 
-    # Train model
-    trainer.train(epochs=args.epochs, valloader=val_loader, log_dir=args.log_dir)
+
+    if args.trades:
+        trainer = train_model.TRADESModelTrainer(model, attacker, train_loader, args.lr, args.optimizer, device=args.device)
+        # Train model
+        trainer.train(epochs=args.epochs, valloader=val_loader, log_dir=args.log_dir, gamma=args.gamma, semi_supervised=args.unsupervised)
+    else:
+        # Get trainer object
+        trainer = train_model.ModelTrainer(model, attacker, train_loader, args.lr, args.optimizer, device=args.device)
+        # Train model
+        trainer.train(epochs=args.epochs, valloader=val_loader, log_dir=args.log_dir)
+        
+
+    trainer.save_model(args.save_path)
 
 if __name__ == "__main__":
     main()
